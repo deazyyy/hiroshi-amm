daeb4ae19a0a6118cf7224ffc48d933ecd26d70e
wanglonghong <wanglonghong4467@gmail.com>
2021-08-31 02:41:01 +0800

    Update priceContracts.ts

--- src/constants/priceContracts.ts
+++ src/constants/priceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {nativeTokenAddress: string, busdAddress: string, lpAddress:string} = {
-  nativeTokenAddress: '0xB95903Cc5bAFE817b98e679E7272FB7b54C105F9',
+  nativeTokenAddress: '0x3d76C3A66c1af77f70a1a530D0A23736977b2E85',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x9e91d6ab938f41611e36c4e50bb0ef5fb575f7f7'
+  lpAddress: '0xcaeb09280b22831dd319ec4e45a013e8ed73fd09'
 }
 
 export default priceContracts
