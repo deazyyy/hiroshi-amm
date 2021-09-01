8800ca12682f64de6a922507a94d69013625bc76
wanglonghong <wanglonghong4467@gmail.com>
2021-09-01 09:29:10 +0800

    change hiro token

--- src/constants/priceContracts.ts
+++ src/constants/priceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {nativeTokenAddress: string, busdAddress: string, lpAddress:string} = {
-  nativeTokenAddress: '0x3d76C3A66c1af77f70a1a530D0A23736977b2E85',
+  nativeTokenAddress: '0x8E92024b5D7A4A3b03cE06b280D776c9C36b64ca',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0xcaeb09280b22831dd319ec4e45a013e8ed73fd09'
+  lpAddress: '0xb993150c6f709c6094064d0d2e0053cc9495ea82'
 }
 
 export default priceContracts
